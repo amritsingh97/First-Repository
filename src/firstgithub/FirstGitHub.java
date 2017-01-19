d9d333e28bc1cce7fda05bbe06df3a9d413354d3
ASUS <ASUS@10.250.12.164>
2017-01-19 11:26:57 -0500

    First Commitment

---
+++ src/firstgithub/FirstGitHub.java
@@ -0,0 +1,10 @@
+package firstgithub;
+
+public class FirstGitHub {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to first Git Hub repository.");
+    }
+   
+}
